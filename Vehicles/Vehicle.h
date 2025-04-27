2cdb6deaad24c5e7ee149f958e959ffdd7413db2
John-E-764 <jeike369@gmail.com>
2025-04-27 18:51:39 -0400

    Added Vehicle class

--- Vehicles/Vehicle.h
+++ Vehicles/Vehicle.h
@@ -1,4 +1,21 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
 
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Vehicle {
+private: 
+	string manufacturer;
+	int year;
+public:
+	Vehicle(string, int);
+	void setManufacturer(string);
+	void setYear(int);
+	string getManufacturer();
+	int getYear();
+	void displayInfo();
+};
+
 #endif
