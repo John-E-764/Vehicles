22459bf63b8a02214e232fdedf0fec768b08f4a7
John-E-764 <jeike369@gmail.com>
2025-04-27 19:13:10 -0400

    Wrong file :(

--- Vehicles/Truck.cpp
+++ Vehicles/Truck.cpp
@@ -1,11 +1,1 @@
-#include "Vehicle.h"
 
-Vehicle::Vehicle(string m, int y) {
-	manufacturer = m;
-	year = y;
-}
-
-void Vehicle::displayInfo() const {
-	cout << "Manufacturer: " << manufacturer << endl;
-	cout << "Year: " << year << endl;
-}
