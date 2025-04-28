f6d67299dce21b95ca30f81f9a8f70077d5677db
John-E-764 <jeike369@gmail.com>
2025-04-28 19:08:16 -0400

    Minor formatting changes

--- Vehicles/Car.h
+++ Vehicles/Car.h
@@ -3,15 +3,15 @@
 
 #include "Vehicle.h"
 
-class Car : public Vehicle{
+class Car : public Vehicle {
 private:
 	int doors;
 public:
 	Car(string, int, int);
-	int getDoors() const{
+	int getDoors() const {
 		return doors;
 	}
-	void setDoors(int d){
+	void setDoors(int d) {
 		doors = d;
 	}
 	void displayInfo() const;
