7a3273a6e0a6018f6cc67e7eeaccc7c1e77dad97
John-E-764 <jeike369@gmail.com>
2025-04-28 19:22:11 -0400

    Created Truck.h

--- Vehicles/Truck.h
+++ Vehicles/Truck.h
@@ -1,4 +1,20 @@
 #ifndef TRUCK_H
 #define TRUCK_H
 
+#include "Vehicle.h"
+
+class Truck : public Vehicle {
+private:
+	int capacity;
+public:
+	Truck(string, int, int);
+	int getCapacity() const {
+		return capacity;
+	}
+	void setCapacity(int c) {
+		capacity = c;
+	}
+	void displayInfo();
+};
+
 #endif
