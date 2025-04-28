95f09dcc9d9aceba731de6caa8afaf2c1cc58e34
John-E-764 <jeike369@gmail.com>
2025-04-28 19:02:04 -0400

    Created Car.h

--- Vehicles/Car.h
+++ Vehicles/Car.h
@@ -1,4 +1,21 @@
 #ifndef CAR_H
 #define CAR_H
 
+class Car : public Vehicle{
+
+private:
+int doors;
+
+public:
+Car(string, int, int);
+int getDoors() const{
+	return doors;
+}
+void setDoors(int d){
+	doors = d;
+}
+void displayInfo() const;
+
+};
+
 #endif
