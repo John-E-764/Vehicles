81c3f00498e33a71cab123b2bb66ee4b25a76d1e
John-E-764 <jeike369@gmail.com>
2025-04-28 19:05:59 -0400

    Added #include

--- Vehicles/Car.h
+++ Vehicles/Car.h
@@ -1,21 +1,20 @@
 #ifndef CAR_H
 #define CAR_H
 
+#include "Vehicle.h"
+
 class Car : public Vehicle{
-
 private:
-int doors;
-
+	int doors;
 public:
-Car(string, int, int);
-int getDoors() const{
-	return doors;
-}
-void setDoors(int d){
-	doors = d;
-}
-void displayInfo() const;
-
+	Car(string, int, int);
+	int getDoors() const{
+		return doors;
+	}
+	void setDoors(int d){
+		doors = d;
+	}
+	void displayInfo() const;
 };
 
 #endif
