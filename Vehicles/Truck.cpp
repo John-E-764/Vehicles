840490e5f2fbc4ba2afa5e83fc0c2df9572166b8
John-E-764 <jeike369@gmail.com>
2025-04-28 19:26:56 -0400

    Created Truck.cpp

--- Vehicles/Truck.cpp
+++ Vehicles/Truck.cpp
@@ -1,1 +1,10 @@
+#include "Truck.h"
 
+Truck::Truck(string m, int y, int c) : Vehicle(m, y) {
+	capacity = c;
+}
+
+void Truck::displayInfo() const {
+	Vehicle::displayInfo();
+	cout << "Towing Capacity (kg): " << capacity << endl;
+}
