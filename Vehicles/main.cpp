1f6861b967b2309b1b6c7188434676ce8b9dc8e5
John-E-764 <jeike369@gmail.com>
2025-04-28 19:40:33 -0400

    Added driver

--- Vehicles/main.cpp
+++ Vehicles/main.cpp
@@ -1,3 +1,43 @@
-//John Eike
+//John E.
 //CIS 1202 501
 //April 27, 2025
+
+#include "Car.h"
+#include "Truck.h"
+#include <iostream>
+#include <string>
+
+int main() {
+	string m;
+	int y, d, c;
+	cout << "Vehicle manufacturer: ";
+	cin >> m;
+	cout << "Vehicle year: ";
+	cin >> y;
+	Vehicle v1(m, y);
+
+	cout << endl;
+	cout << "Car manufacturer: ";
+	cin >> m;
+	cout << "Car year: ";
+	cin >> y;
+	cout << "Number of doors: ";
+	cin >> d;
+	Car v2(m, y, d);
+
+	cout << endl;
+	cout << "Truck manufacturer: ";
+	cin >> m;
+	cout << "Truck year: ";
+	cin >> y;
+	cout << "Towing capacity: ";
+	cin >> c;
+	Truck v3(m, y, c);
+
+	cout << endl;
+	v1.displayInfo();
+	cout << endl;
+	v2.displayInfo();
+	cout << endl;
+	v3.displayInfo();
+}
